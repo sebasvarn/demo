984d22b521266029383315fddf5ef725b5f5b9b0
camper-py <srsebas101@gmail.com>
2023-10-03 22:52:24 -0300

    V2 SQL IMPLEMENTATION

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class DemoApplication {
 
     public static void main(String[] args) {
