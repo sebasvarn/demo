3569b0d1c6fb8a2b4a5432a47c4457d8f1b9df98
camper-py <srsebas101@gmail.com>
2023-10-04 16:08:32 -0300

    V2 SQL IMPLEMENTATION

--- src/main/java/com/example/demo/Student/Student.java
+++ src/main/java/com/example/demo/Student/Student.java
@@ -1,8 +1,21 @@
 package com.example.demo.Student;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private Integer age;
@@ -22,6 +35,10 @@ public class Student {
         this.age = age;
         this.dov = dov;
         this.email = email;
+    }
+
+    public Student() {
+
     }
 
     public Long getId() {
